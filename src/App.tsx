37a3190063de8911a253f4cc49cfd40df0382114
3myr <remy.demazieres.store@gmail.com>
2025-03-02 19:23:30 +0100

    Init boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+      <View className="flex-1 items-center justify-center bg-gray-100">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to the Boilerplate !
+        </Text>
+      </View>
+  );
+}
